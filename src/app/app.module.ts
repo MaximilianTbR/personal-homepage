d32bb188bc435346525023bfe7b60946fecb09ec
Maximilian Tauber <maximilian.tauber@icloud.com>
2022-10-25 06:59:38 +0200

    workingon first section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FirstSectionComponent } from './first-section/first-section.component';
+import { SecondSectionComponent } from './second-section/second-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FirstSectionComponent,
+    SecondSectionComponent
   ],
   imports: [
     BrowserModule,
