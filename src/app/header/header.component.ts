1ce9a531bb46ccba713af654ae3514c30949d8c4
Maximilian Tauber <maximilian.tauber@icloud.com>
2022-11-27 11:34:30 +0100

    working on section 2

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -12,4 +12,16 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  BL = false;
+
+  openMenu() {
+    if (!this.BL) {
+      document.getElementById('open-menu')?.classList.remove('d-none');
+      this.BL = true;
+    } else {
+      document.getElementById('open-menu')?.classList.add('d-none');
+      this.BL = false;
+    }
+  }
+
 }
