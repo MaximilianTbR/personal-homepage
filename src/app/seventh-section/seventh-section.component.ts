a4dc475560d09becdb2fd98d6252768893d9a1ac
Maximilian Tauber <maximilian.tauber@icloud.com>
2022-12-01 20:19:33 +0100

    finished first functional version of contact form

--- src/app/seventh-section/seventh-section.component.ts
+++ src/app/seventh-section/seventh-section.component.ts
@@ -7,13 +7,38 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 })
 export class SeventhSectionComponent implements OnInit {
   @ViewChild('myForm') myForm!: ElementRef;
+  @ViewChild('nameField') nameField!: ElementRef;
+  @ViewChild('mailField') mailField!: ElementRef;
+  @ViewChild('messageField') messageField!: ElementRef;
+  @ViewChild('inputBtn') inputBtn!: ElementRef;
+
 
   ngOnInit(): void {
 
   }
 
-  sendMail() {
-    //action="https://maximilian-tauber.developerakademie.net/send_mail/send_mail.php"
+  async sendMail() {
     console.log('sending mail!', this.myForm);
+    let nameField = this.nameField.nativeElement;
+    let emailField = this.mailField.nativeElement;
+    let messageField = this.messageField.nativeElement;
+    nameField.disabled = true;
+    emailField.disabled = true;
+    messageField.disabled = true;
+    // Animation
+    let fd = new FormData();
+    fd.append('name', nameField.value);
+    fd.append('email', emailField.value);
+    fd.append('message', messageField.value);
+
+
+    await fetch('https://maximilian-tauber.developerakademie.net/send_mail/send_mail.php', {
+      method: 'POST',
+      body: fd
+    });
+    // show text: message sent!
+    nameField.disabled = false;
+    emailField.disabled = false;
+    messageField.disabled = false;
   }
 }
