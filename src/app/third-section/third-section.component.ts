780c8cd95508b26de39e412557d346eaac32e8cd
Maximilian Tauber <maximilian.tauber@icloud.com>
2022-12-11 12:08:37 +0100

    working on animations

--- src/app/third-section/third-section.component.ts
+++ src/app/third-section/third-section.component.ts
@@ -1,27 +1,24 @@
 import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+//import AOS from 'aos';
+//import 'aos/dist/aos.css'; // You can also use <link> for styles
+import * as Aos from 'aos';
+
 
 @Component({
   selector: 'app-third-section',
   templateUrl: './third-section.component.html',
   styleUrls: ['./third-section.component.scss'],
   animations: [
-    trigger('myInsertTrigger', [
-      transition('void => *', [
-        animate('fadeInOut 1s ease-in-out linear')
-      ])
-    ])
   ]
 })
 export class ThirdSectionComponent implements OnInit {
 
   constructor() {
-    AOS.init();
   }
 
   ngOnInit(): void {
+    Aos.init();
   }
 
 }
